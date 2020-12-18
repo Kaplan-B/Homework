6a34f83fa42eaa7a57aca84326dfd1227c8e08dc
Артём Бармин <artyom.barmin@ya.ru>
2020-12-18 13:40:56 +0300

    Добавил перегрузку оператора,конструктор копирования

--- antenna.cpp
+++ antenna.cpp
@@ -2,5 +2,8 @@
 
 Antenna::Antenna()
 {
-
+     frequency_range = "2400~2500";
+     antenna_gain = 9;
+     type_WIFI = "¬нешн€€ всенаправленна€";
+     resistance = 50;
 }
--- antenna.hpp
+++ antenna.hpp
@@ -1,11 +1,19 @@
 #ifndef ANTENNA_HPP
 #define ANTENNA_HPP
-
+#include <string>
+using namespace std;
 
 class Antenna
 {
 public:
-    Antenna();
+    Antenna(); // конструктор по умолчанию
+    Antenna(int frequency_range,int antenna_gain,string type_WIFI,int resistance); //конструктор инициализации
+    ~Antenna();//деструктор
+protected:
+    int frequency_range;
+    int antenna_gain;
+    string type_WIFI;
+    int resistance;
 };
 
 #endif // ANTENNA_HPP
