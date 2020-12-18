6e5484598f16d8b29fb0f26ee7394565c514f004
Артём Бармин <artyom.barmin@ya.ru>
2020-12-18 14:40:08 +0300

    Начал описание для Lumax

--- lumax.cpp
+++ lumax.cpp
@@ -1,6 +1,13 @@
 #include "lumax.hpp"
-
+#include <string>
+using namespace std;
 Lumax::Lumax()
 {
-
+    frequency_range = "2400~2500";
+    antenna_gain = 9;
+    type_WIFI = "Внешняя всенаправленная";
+    resistance = 50;
+    operating_temperature = 50;
+    antenna_connectors = 1;
+    polarization = "Вертикальная и горизонтальная";
 }
--- lumax.hpp
+++ lumax.hpp
@@ -1,11 +1,18 @@
 #ifndef LUMAX_HPP
 #define LUMAX_HPP
-
-
-class Lumax
+#include "antenna.hpp"
+#include <string>
+using namespace std;
+class Lumax: public Antenna
 {
 public:
     Lumax();
+    Lumax(string frequency_range,int antenna_gain,string type_WIFI,int resistance);
+    ~Lumax();
+private:
+   int operating_temperature;
+   int antenna_connectors;
+   string polarization;
 };
 
 #endif // LUMAX_HPP
